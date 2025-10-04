eb76bfa05ff663600bf1474908ea3e4e860ee1bf
Schmexus <max.shincko@yandex.ru>
2025-10-04 19:29:31 +0700

    go back

--- tests/test_client_api.py
+++ tests/test_client_api.py
@@ -13,7 +13,7 @@ def test_get_users(api_client):
 
 def test_wrong_EP(api_client):
     response = api_client.get("test/users?page=2", headers={"Accept": "application/json", "x-api-key": "reqres-free-v1"})
-    assert response.status_code == 200
+    assert response.status_code == 404
 
 def test_post(api_client):
     response = api_client.post("users", headers={"Accept": "application/json", "x-api-key": "reqres-free-v1"}, data={'name': 'John Doe', 'job': 'Developer'})
